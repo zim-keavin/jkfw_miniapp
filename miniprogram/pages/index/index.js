d697758b0dd22647a2ff7aa5562f0b4290501aed
zim-keavin <2286827822@qq.com>
2020-04-11 21:16:52 +0800

    完善首页框架

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,12 +3,17 @@ const app = getApp()
 
 Page({
   data: {
-    imgUrls:[
+    imgUrls: [
       '/images/3.jpg',
       '/images/2.jpg',
       '/images/1.jpg',
     ],
-    
+    article: [{
+      title: "这里是标题",
+      content: "这里是副标题",
+      photo:"../../images/1.jpg"
+    }]
+
   },
 
   onLoad: function() {
